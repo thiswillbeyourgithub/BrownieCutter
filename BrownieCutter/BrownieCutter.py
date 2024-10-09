127b9bc33061cc53b290368d9481a1097b0e3287
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-10-09 16:51:39 +0200

    bump version 0.1.10 -> 0.1.11

--- BrownieCutter/BrownieCutter.py
+++ BrownieCutter/BrownieCutter.py
@@ -6,7 +6,7 @@ import os
 
 @beartype  # this will apply to all methods
 class BrownieCutter:
-    VERSION: str = "0.1.10"
+    VERSION: str = "0.1.11"
 
     def __init__(self) -> None:
         """
